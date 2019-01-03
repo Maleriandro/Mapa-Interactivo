361b3b371ecd741ff28ae85d2c85c8d73cf086b9
Maleriandro <mariano.lazza@outlook.com>
2019-01-03 20:42:38 -0300

    guia 1 completada

--- js/apis.js
+++ js/apis.js
@@ -1,9 +1,18 @@
 var mapa // Mapa que vamos a modificar
 
+var posicionCentral = {
+  lat: -34.673013,
+  lng: -58.473898
+}
+
 /* Crear la variable posicionCentral con las coordenadas donde se va a centrar el mapa */
 
 // Inicializa el mapa con un valor de zoom y una locación en el medio
 function inicializarMapa () {
+  mapa = new google.maps.Map(document.getElementById("map"), {
+    center: posicionCentral,
+    zoom: 13
+  });
     /* Modificá la variable mapa con el constructor Map().
     Tendrás que asignarle un valor de zoom y
     un centro igual a la variable posicionCentral. */
